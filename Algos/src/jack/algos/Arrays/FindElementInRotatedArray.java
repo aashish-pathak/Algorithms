30049c7d2e05eb823b3352db3ba4f026428d9ff1
Ketan Akade <ketanakade@gmail.com>
2016-02-07 13:31:33 -0800

    Find Element In Rotated Array

---
+++ Algos/src/jack/algos/Arrays/FindElementInRotatedArray.java
@@ -0,0 +1,40 @@
+package jack.algos.Arrays;
+
+import jack.algos.Search.Binary;
+
+public class FindElementInRotatedArray {
+	public static void main(String[] args) {
+		int in[] = { 2,6,8,9,15,20,18,16,12,7,1};
+		int num = 12;
+		Binary b = new Binary();
+		//get the highest element in the array
+		//This will be the point where array can be divided into 2 subarrays
+		//O(logn) can be then used to find num in these 2 subarrays
+		//O(log n + log n) = O(log n)
+		
+		int maxIndex = getMaxIndex(in);
+		//System.out.println("Maxindex: " + maxIndex);
+		int numIndex = Binary.search(in, num, 0, maxIndex);
+		if (numIndex == -1) 
+			numIndex = Binary.searchDesc(in, num, maxIndex+1, in.length-1);
+		
+		System.out.println("Number " + num + " found at index: " + numIndex);
+	}
+	
+	public static int getMaxIndex(int[] in) {
+		int l = in.length;
+		int low = 0, high = l-1;
+		int mid = (low+high)/2;
+		while(low<high) {
+			if(in[mid] > in[mid-1]) {
+				low = mid;
+			} else {
+				high = mid;
+			}
+			mid = (low+high)/2;
+			if ((mid == low) || (mid == high))
+				break;
+		}
+		return mid;
+	}
+}
--- Algos/src/jack/algos/Search/Binary.java
+++ Algos/src/jack/algos/Search/Binary.java
@@ -3,7 +3,7 @@ package jack.algos.Search;
 public class Binary {
 	public static void main(String[] args) {
 		int[] in = {1,2,3,4,5,6,7,8,9};
-		int key = 1;
+		int key = 5;
 		int low=0, high=in.length - 1;
 		int retVal = search(in, key, low, high);
 		if (retVal == -1)
@@ -12,6 +12,7 @@ public class Binary {
 			System.out.println("Element found at position: " + retVal);
 	}
 	
+	//Assuming the array is sorted in ascending order
 	public static int search(int[] in, int key, int low, int high) {
 		int mid;
 		if (low <= high) {
@@ -25,4 +26,20 @@ public class Binary {
 		}
 		return -1;
 	}
+	
+	//For the array sorted in desc order
+	public static int searchDesc(int[] in, int key, int low, int high) {
+		int mid;
+		if (low <= high) {
+			mid = (low + high )/ 2;
+			if (key == in[mid])
+				return mid;
+			else if (key < in[mid]) 
+				return searchDesc(in, key, mid+1, high);
+			else if (key > in[mid])
+				return searchDesc(in, key, low, mid-1);
+		}
+		return -1;
+	}
 }
+//18,16,12,7,1
