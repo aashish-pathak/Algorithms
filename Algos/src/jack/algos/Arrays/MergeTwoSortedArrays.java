238ea55294650a237595e00babb23e7f7318d563
Ketan Akade <ketanakade@gmail.com>
2016-02-21 14:12:49 -0800

    Merging 2 sorted arrays with O(1) space complexity

---
+++ Algos/src/jack/algos/Arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,48 @@
+/*We are given two sorted array. 
+ * We need to merge these two arrays such that 
+ * the initial numbers (after complete sorting) are in the first array and the remaining numbers are in the second array. 
+ * Extra space allowed in O(1).
+
+Example:
+
+Input: ar1[] = {1, 5, 9, 10, 15, 20};
+       ar2[] = {2, 3, 8, 13};
+Output: ar1[] = {1, 2, 3, 5, 8, 9}
+        ar2[] = {10, 13, 15, 20}  
+*/
+package jack.algos.Arrays;
+
+import jack.algos.Search.Binary;
+
+public class MergeTwoSortedArrays {
+	static int[] arr1 = {1, 5, 9, 10, 15, 20};
+	static int[] arr2 = {2, 3, 8, 13};
+	public static void main(String[] args) {
+		merge();
+		
+		System.out.println("Array1:");
+		for(int i:arr1)
+			System.out.print(i + " ");
+		System.out.println("\nArray2:");
+		for(int i:arr2)
+			System.out.print(i + " ");
+	}
+	
+	public static void merge() {
+		int l1, l2;
+		l1 = arr1.length;
+		l2 = arr2.length;
+		for(int i=l2-1; i>=0; i--) {
+			int elementToShift = arr2[i];
+			int indexInArr1 = Binary.indexToInsert(arr1, elementToShift, 0, l1-1);
+			arr2[i] = arr1[l1-1];
+			shiftArrayFromGivenIndex(indexInArr1);
+			arr1[indexInArr1] = elementToShift;
+		}
+	}
+	//this function shifts the array values by 1 position to the right from given index
+	public static void shiftArrayFromGivenIndex(int index) {
+		for(int i=arr1.length-1; i> index; i--) 
+			arr1[i] = arr1[i-1];
+	}
+}
--- Algos/src/jack/algos/Search/Binary.java
+++ Algos/src/jack/algos/Search/Binary.java
@@ -3,6 +3,8 @@ package jack.algos.Search;
 public class Binary {
 	public static void main(String[] args) {
 		int[] in = {1,2,3,4,5,6,7,8,9};
+		//int[] in = {2,4,6,8,10};
+		//int key = 11;
 		int key = 5;
 		int low=0, high=in.length - 1;
 		int retVal = search(in, key, low, high);
@@ -40,6 +42,21 @@ public class Binary {
 				return searchDesc(in, key, low, mid-1);
 		}
 		return -1;
+	}
+	
+	//if a number is not present in the array, find the position where it can be inserted
+	public static int indexToInsert(int[] in, int key, int low, int high) {
+		int mid;
+		if (low <= high) {
+			mid = (low + high )/ 2;
+			if (key == in[mid])
+				return mid;
+			else if (key < in[mid])
+				return indexToInsert(in, key, low, mid-1);
+			else if (key > in[mid])
+				return indexToInsert(in, key, mid+1, high);
+		}
+		return (low > high) ? low : high;
 	}
 }
 //18,16,12,7,1
